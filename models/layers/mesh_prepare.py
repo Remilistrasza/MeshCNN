bf545ee8f791a2d03b410ab348c15e9d40fac287
Yingqiang Liu <53621227+Remilistrasza@users.noreply.github.com>
2022-12-03 04:42:39 -0700

    Update mesh_prepare.py

--- models/layers/mesh_prepare.py
+++ models/layers/mesh_prepare.py
@@ -341,10 +341,10 @@ def extract_features(mesh):
 
 def edge_centroid(mesh, edge_points):
     centroid = (mesh.vs[edge_points[:, 0]] + mesh.vs[edge_points[:, 1]]) / 2
-    print(type(centroid[0][0]))
+    #print(type(centroid[0][0]))
     #centroid = np.concatenate((np.expand_dims(centroid[:][0], 0)), axis=0)
     centroid = np.expand_dims(centroid, axis=2)
-    print(centroid.shape)
+    #print(centroid.shape)
     #centroid = np.sum(centroid, axis=1)
     return centroid
 ############## End of new features ############
