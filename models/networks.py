34d2d4fc4c20a9a7bdc6e39360d390a31169f427
Yingqiang Liu <53621227+Remilistrasza@users.noreply.github.com>
2022-12-03 02:49:18 -0700

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -131,7 +131,7 @@ class MeshConvNet(nn.Module):
         super(MeshConvNet, self).__init__()
         self.k = [nf0] + conv_res
         #self.res = [input_res] + pool_res
-        self.res = [input_res, input_res, input_res, input_res]
+        self.res = [input_res]
         norm_args = get_norm_args(norm_layer, self.k[1:])
 
         for i, ki in enumerate(self.k[:-1]):
@@ -139,12 +139,12 @@ class MeshConvNet(nn.Module):
             setattr(self, 'norm{}'.format(i), norm_layer(**norm_args[i]))
 
             #removing pooling layer
-            setattr(self, 'pool{}'.format(i), MeshPool(self.res[i + 1]))
+            #setattr(self, 'pool{}'.format(i), MeshPool(self.res[i + 1]))
             #setattr(self, 'pool{}'.format(i), numpy.array(input_res))
             #####
 
         #removing pooling layer
-        self.gp = torch.nn.AvgPool1d(self.res[-1])
+        #self.gp = torch.nn.AvgPool1d(self.res[-1])
         ####
 
         # self.gp = torch.nn.MaxPool1d(self.res[-1])
@@ -158,11 +158,11 @@ class MeshConvNet(nn.Module):
             x = F.relu(getattr(self, 'norm{}'.format(i))(x))
 
             #removing pooling layer
-            x = getattr(self, 'pool{}'.format(i))(x, mesh)
+            #x = getattr(self, 'pool{}'.format(i))(x, mesh)
             ######
 
         #removing pooling layer
-        x = self.gp(x)
+        #x = self.gp(x)
         x = x.view(-1, self.k[-1])
         #######
 
